891d73d026f1fb2ebeab1b360fc672333ca65013
StevenASam <steven.sam68@gmail.com>
2022-06-01 20:25:17 -0700

    bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-import React, {useEffect, useState} from 'react'
-import {Layout} from '../components'
+import React from 'react';
 
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.scss'
+import '../styles/globals.scss';
+import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {... pageProps} />
+      <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
